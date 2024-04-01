6bea76fb0ed57070cecf6acd5c15c39a4035a14f
kungran <kungrankang@gmail.com>
2024-04-01 10:22:32 +0900

    머지오류 다시 푸시

---
+++ js/ran.js
@@ -0,0 +1,152 @@
+window.addEventListener("load", function () {
+  // 쇼츠 기능
+  // 첫번째 모달 창 열기
+  document.getElementById("youtube-link").addEventListener("click", function () {
+    document.getElementById("youtube-modal").style.display = "block";
+  });
+
+  // 첫 번째 모달 창 닫기
+  document.querySelectorAll("#youtube-modal .s-close").forEach(function (element) {
+    element.addEventListener("click", function () {
+      document.getElementById("youtube-modal").style.display = "none";
+    });
+  });
+
+  // 두번째 모달 창 열기
+  document.getElementById("youtube-link-2").addEventListener("click", function () {
+    document.getElementById("youtube-modal-2").style.display = "block";
+  });
+
+  // 두 번째 모달 창 닫기
+  document.querySelectorAll("#youtube-modal-2 .s-close").forEach(function (element) {
+    element.addEventListener("click", function () {
+      document.getElementById("youtube-modal-2").style.display = "none";
+    });
+  });
+
+  // 세번째 모달 창 열기
+  document.getElementById("youtube-link-3").addEventListener("click", function () {
+    document.getElementById("youtube-modal-3").style.display = "block";
+  });
+  // 세번째 모달 창 닫기
+  document.querySelectorAll("#youtube-modal-3 .s-close").forEach(function (element) {
+    element.addEventListener("click", function () {
+      document.getElementById("youtube-modal-3").style.display = "none";
+    });
+  });
+
+  // 네번째 모달 창 열기
+  document.getElementById("youtube-link-4").addEventListener("click", function () {
+    document.getElementById("youtube-modal-4").style.display = "block";
+  });
+  // 네번째 모달 창 닫기
+  document.querySelectorAll("#youtube-modal-4 .s-close").forEach(function (element) {
+    element.addEventListener("click", function () {
+      document.getElementById("youtube-modal-4").style.display = "none";
+    });
+  });
+
+  // 다섯번째 모달 창 열기
+  document.getElementById("youtube-link-5").addEventListener("click", function () {
+    document.getElementById("youtube-modal-5").style.display = "block";
+  });
+  // 다섯번째 모달 창 닫기
+  document.querySelectorAll("#youtube-modal-5 .s-close").forEach(function (element) {
+    element.addEventListener("click", function () {
+      document.getElementById("youtube-modal-5").style.display = "none";
+    });
+  });
+
+  // 여섯번째 모달 창 열기
+  document.getElementById("youtube-link-6").addEventListener("click", function () {
+    document.getElementById("youtube-modal-6").style.display = "block";
+  });
+  // 여섯번째 모달 창 닫기
+  document.querySelectorAll("#youtube-modal-6 .s-close").forEach(function (element) {
+    element.addEventListener("click", function () {
+      document.getElementById("youtube-modal-6").style.display = "none";
+    });
+  });
+
+  //쇼츠 닫기버튼 클릭시 계속 재생되는 문제 해결
+  $(document).ready(function () {
+    // 모든 모달에 대한 참조를 담을 객체
+    var modals = {};
+    // 모달 ID들을 배열로 초기화
+    var modalIds = ["youtube-modal", "youtube-modal-2", "youtube-modal-3", "youtube-modal-4", "youtube-modal-5", "youtube-modal-6"];
+    // 각 모달 ID에 대해 반복하며 modals 객체에 jQuery 객체로 저장
+    $.each(modalIds, function (index, value) {
+      modals[value] = $("#" + value);
+    });
+    // 모든 ".s-close" 버튼에 대한 클릭 이벤트 핸들러
+    $(".s-close").click(function () {
+      // 클릭된 버튼이 속한 모달의 ID를 찾음
+      var modalId = $(this).closest(".s-modal").attr("id");
+      // 해당 모달 내의 iframe의 src 속성을 빈 문자열로 설정
+      modals[modalId].find("iframe").attr("src", "");
+      // 선택적: 해당 모달을 숨김
+      // modals[modalId].hide();
+    });
+  });
+
+  // FAQ 모달 창 열기
+  document.getElementById("FAQ-btn").addEventListener("click", function () {
+    document.getElementById("FAQ-modal").style.display = "block";
+  });
+  // FAQ 모달 창 닫기
+  document.querySelectorAll("#FAQ-modal .q-m-close").forEach(function (element) {
+    element.addEventListener("click", function () {
+      document.getElementById("FAQ-modal").style.display = "none";
+    });
+  });
+  // FAQ 모달 아코디언 메뉴기능
+  $(document).ready(function () {
+    $(".accordion-header").click(function () {
+      $(this).next(".accordion-content").slideToggle();
+      $(".accordion-content").not($(this).next()).slideUp();
+    });
+  });
+
+  //   신청조회 모달 창 열기
+  document.getElementById("search-btn").addEventListener("click", function () {
+    document.getElementById("search-modal").style.display = "block";
+  });
+  //   신청조회 모달 창 닫기
+  document.querySelectorAll("#search-modal .q-m-close").forEach(function (element) {
+    element.addEventListener("click", function () {
+      document.getElementById("search-modal").style.display = "none";
+    });
+  });
+
+  //   top 버튼 스크롤 기능
+  const topBtn = document.getElementById("top-btn");
+
+  topBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    console.log(window.scrollY);
+
+    if (window.scrollY === 0) {
+      window.scrollTo({
+        top: 1050, //맨 아래 스크롤값 2952
+        behavior: "smooth",
+      });
+    } else {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  });
+
+  // 화살표 이미지 회전
+  const topBtnImg = document.getElementById("top-btn-img");
+  window.addEventListener("scroll", function (scTop) {
+    scTop = window.document.documentElement.scrollTop;
+    if (scTop > 0) {
+      topBtnImg.classList.add("up");
+    } else {
+      topBtnImg.classList.remove("up");
+    }
+  });
+});
---
+++ js/sub.js
@@ -0,0 +1,162 @@
+window.onload = function () {
+  //min
+  //menu area
+  //topmenu
+  // index 페이지의 메뉴 항목을 찾아서 해당 항목에 active 클래스를 추가합니다.
+  const currentPage = "sub.html"; // 현재 페이지의 URL을 설정합니다.
+  const topMenu = document.querySelector(".top-menu");
+  const topMenuLi = document.querySelectorAll(".top-menu > li");
+
+  topMenuLi.forEach((li) => {
+    const link = li.querySelector("a"); // 각 li 요소의 하위 a 요소를 찾습니다.
+    const href = link.getAttribute("href"); // a 요소의 href 속성 값을 가져옵니다.
+
+    // 만약 현재 페이지의 URL이 해당 li 요소의 href 속성 값과 일치한다면 active 클래스를 추가합니다.
+    if (href === currentPage) {
+      li.classList.add("active");
+    }
+  });
+
+  topMenu.addEventListener("mouseleave", () => {
+    topMenuLi.forEach((item) => {
+      const link = item.querySelector("a");
+      const href = link.getAttribute("href");
+      if (href === currentPage) {
+        item.classList.add("active");
+      } else {
+        item.classList.remove("active");
+      }
+    });
+  });
+
+  topMenuLi.forEach((li) => {
+    li.addEventListener("click", () => {
+      // 클릭된 li에만 active 클래스를 추가하고 다른 li에서는 제거합니다.
+      topMenuLi.forEach((item) => {
+        if (item === li) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
+        }
+      });
+    });
+
+    li.addEventListener("mouseover", () => {
+      // 호버한 메뉴 항목에 .active 클래스를 추가하고 이전에 추가된 클래스를 제거합니다.
+      topMenuLi.forEach((item) => {
+        if (item === li) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
+        }
+      });
+    });
+  });
+  //top menu scroll logo hidden
+  const topLogo = document.querySelector(".logo");
+  const langBox = document.querySelector(".lang");
+  let scy = 0;
+
+  window.addEventListener("scroll", function () {
+    // 새로 고침 / url 입력해서 html 출력시
+    // 1.스크롤바의 픽셀 위치값을 파악해서
+    scy = this.document.documentElement.scrollTop;
+    const innerWidth = window.innerWidth;
+    // 2.class 적용 여부 결정
+    if (innerWidth > 780) {
+      if (scy > 0) {
+        topLogo.classList.add("logoh");
+        langBox.classList.add("logoh");
+      } else {
+        topLogo.classList.remove("logoh");
+        langBox.classList.remove("logoh");
+      }
+    }
+  });
+  //language
+  const langToggle = document.querySelector(".lang-more");
+  const langM = document.querySelector(".lang > li:last-of-type");
+  langToggle.addEventListener("click", () => {
+    langM.classList.toggle("show");
+  });
+  langM.addEventListener("click", () => {
+    langM.classList.remove("show");
+  });
+  //qna swiper
+  //반응형 스와이퍼
+
+  let ww = window.innerWidth;
+  let swQna = null; // null로 초기화
+
+  responsiveSwiper();
+
+  function initSwiper(effect) {
+    if (swQna !== null) {
+      swQna.destroy(); // 기존 슬라이더가 존재하면 파괴
+    }
+
+    let slidesPerViewValue = 1; // 슬라이드 당 보여질 개수 기본값
+    let spaceBetweenValue = 20; // 슬라이드 사이의 간격 기본값
+
+    if (ww < 780) {
+      slidesPerViewValue = 1; // 980px 미만일 때 1.1개의 슬라이드를 보여줌
+      spaceBetweenValue = 20; // 980px 미만일 때 슬라이드 사이의 간격을 10px로 설정
+    }
+
+    swQna = new Swiper(".sw-qna", {
+      fadeEffect: { crossFade: true },
+      speed: 500,
+      loop: false,
+      slidesPerView: slidesPerViewValue, // slidesPerView 설정
+      spaceBetween: spaceBetweenValue,
+      on: {
+        slideChange: function () {
+          const idx = this.realIndex;
+          for (let menu of menus) {
+            menu.classList.remove("act");
+          }
+          menus[idx].classList.add("act");
+        },
+      },
+      effect: effect,
+    });
+
+    return swQna;
+  }
+
+  function responsiveSwiper() {
+    if (ww < 780) {
+      initSwiper("slide");
+    } else if (ww >= 780) {
+      initSwiper("fade");
+    }
+  }
+
+  window.addEventListener("resize", function () {
+    ww = window.innerWidth;
+    responsiveSwiper();
+  });
+
+  const menus = document.querySelectorAll(".sub-qna-wrapper .qna-click-list .fn-qcl");
+  function getElementIndex(element) {
+    return Array.from(element.parentElement.children).indexOf(element);
+  }
+  // 메뉴 항목에 대한 이벤트 핸들러 설정
+  menus.forEach((menu, idx) => {
+    menu.addEventListener("mouseover", () => {
+      const fnMenuActive = document.querySelector(".qna-menu-list > li.qna-menu-list-focus");
+
+      if (fnMenuActive) {
+        fnMenuActive.classList.remove("qna-menu-list-focus"); // 기존에 활성화된 메뉴를 비활성화합니다.
+      }
+
+      const currentMenu = document.querySelectorAll(".qna-menu-list > li")[idx];
+      currentMenu.classList.add("qna-menu-list-focus"); // 현재 메뉴를 활성화합니다.
+      swQna.slideTo(idx);
+    });
+  });
+
+  // ran
+
+  // hun
+};
